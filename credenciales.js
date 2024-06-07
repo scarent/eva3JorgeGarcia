18ceeab769ab3973d27c3737ac153a35974925a4
scarent <jorgeee.g.a@gmail.com>
2024-06-07 08:38:14 -0400

    Initial commit

---
+++ credenciales.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVNaSISqiSopFx7ksurCH8khDJ3ewIWkI",
+    authDomain: "eva3pizza.firebaseapp.com",
+    projectId: "eva3pizza",
+    storageBucket: "eva3pizza.appspot.com",
+    messagingSenderId: "342855284059",
+    appId: "1:342855284059:web:2979916e6e7e9c0e1e167e"
+  };
---
+++ firebase.js
@@ -0,0 +1,3 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const app = initializeApp(firebaseConfig);
