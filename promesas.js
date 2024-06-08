9a1a5ede6267b98bef38bd5981b27825875f1a2a
scarent <jorgeee.g.a@gmail.com>
2024-06-08 19:32:58 -0400

    registro/actualizar
    se registran correctamente todos los campos en firebase, lo mismo con actualizar, se actualiza correctamente todo

---
+++ promesas.js
@@ -0,0 +1,27 @@
+import {collection,addDoc, getDocs,updateDoc,deleteDoc,doc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {db} from "./firebase.js";
+export const registrarreserva=async(persona)=>{
+    console.log("persona")
+    console.log(persona)
+    const docRef = await addDoc(collection(db,"persona"),persona);
+}
+export const obtenerPersonas = async()=>{
+    const ref = collection(db,"persona");
+    const quearySnap = await getDocs(ref)
+    console.log(quearySnap)
+    let listado =[]
+    quearySnap.forEach(doc =>{
+        listado.push({...doc.data(),id:doc.id}); //id a los objetos
+
+    });
+    return listado;
+}
+export const actualizarPersona = async(objeto,id)=>{
+    const ref = doc(db,"persona",id);
+    await updateDoc(ref,objeto);
+}
+export const eliminarPersona = async(id)=>{
+    const ref = doc(db,"persona",id);
+    await deleteDoc(ref);
+
+}
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+import { actualizarPersona, eliminarPersona, obtenerPersonas, registrarreserva } from "./promesas.js";
 window.addEventListener("load",()=>{
     document.getElementById("btnmodoscuro").addEventListener("click",cambiarmodo);
     document.getElementById("btnfuente").addEventListener("click",cambiarfuente);
     document.getElementById("btnregistro").addEventListener("click",validar);
-
+    document.getElementById("btnregistro").addEventListener("click",registrar);
+    document.getElementById("btnACTUALIZA").addEventListener("click",actualizar);
+    cargarDatos()
 })
 function cambiarmodo(){
     var element =document.body;
@@ -46,4 +49,135 @@ function validarlargo(campo,largo){
         let pelemento=document.getElementById('p'+campo);
         pelemento.style.display="none"
     }
+}
+const registrar=()=>{
+    //recupero datos
+    let enombre=document.getElementById("nombre")
+    let eemail=document.getElementById("email")
+    let etelefono=document.getElementById("telefono")
+    let efecha=document.getElementById("fecha")
+    let etiporeserva=document.getElementById("tiporeserva")
+    let eacompañante;
+    if(document.getElementById("acompañante1").checked){  //verifica con el checked 
+        eacompañante="1-2 personas"
+    }else if(document.getElementById("acompañante2").checked){
+        eacompañante="3 o mas personas"
+    }
+    let ecomentarios = document.getElementById("comentario")
+    let emetodo;
+    if(document.getElementById("contacto").checked){
+        emetodo="Email";
+    }else if(document.getElementById("contacto2").checked){
+        emetodo="telefono";
+    }
+    
+    //recupero valores
+    let vnombre = enombre.value;
+    let vemail = eemail.value;
+    let vtelefono = etelefono.value;
+    let vfecha = efecha.value;
+    let vtiporeserva = etiporeserva.value;
+    let vacompañante = eacompañante;
+    let vcomentarios = ecomentarios.value;
+    let vmetodo = emetodo;
+    // objeto
+    let objeto={nombre:vnombre,email:vemail,telefono:vtelefono,fecha:vfecha,tiporeserva:vtiporeserva,acompañante:vacompañante,
+        comentarios:vcomentarios,metodo:vmetodo
+    }
+    console.log(objeto)
+    //validacion del registro 
+    registrarreserva(objeto).then(()=>{
+        alert("registrado con exito");
+        cargarDatos();
+    }).catch((r)=>{
+        alert("Algo ocurrio intentelo nuevamnete");
+        alert(r);
+    });
+}
+const cargarDatos = ()=>{
+    obtenerPersonas().then((personas)=>{
+        console.log("recuperado")
+        let estructura="";
+        personas.forEach((persona)=>{
+            estructura +="<tr>"
+            estructura +="<td>"+persona.nombre+"</td>";
+            estructura +="<td>"+persona.email+"</td>";
+            estructura +="<td>"+persona.telefono+"</td>";
+            estructura +="<td>"+persona.fecha+"</td>";
+            estructura +="<td>"+persona.tiporeserva+"</td>";
+            estructura +="<td>"+persona.acompañante+"</td>";
+            estructura +="<td>"+persona.comentario+"</td>";
+            estructura +="<td>"+persona.contactoo+"</td>";
+            estructura += "<td><button id='UPD"+persona.id+"'>Actualizar</button></td>";
+            estructura += "<td><button id='DEL"+persona.id+"'>Eliminar</button></td>";
+            estructura += "</tr>"
+        });
+        document.getElementById("tbDatos").innerHTML=estructura;
+        personas.forEach((persona)=>{
+            let botonUPD =document.getElementById("UPD"+persona.id);
+            botonUPD.addEventListener("click",()=>{
+                let enombre=document.getElementById("UPDnombre");
+                let eemail=document.getElementById("UPDemail");
+                let etelefono=document.getElementById("UPDtelefono");
+                let efecha=document.getElementById("UPDfecha");
+                let etiporeserva=document.getElementById("UPDtiporeserva");
+                let eacompañante=document.getElementById("UPDacompañante")
+                let ecomentarios=document.getElementById("UPDcomentario");
+                let emetodo = document.getElementById("UPDcontactoo")
+                enombre.value=persona.nombre;
+                eemail.value=persona.email;
+                etelefono.value=persona.telefono;
+                efecha.value=persona.fecha;
+                etiporeserva=persona.tiporeserva;
+                eacompañante = persona.acompañante;
+                ecomentarios.value=persona.comentario;
+                emetodo = persona.contactoo
+                document.getElementById("btnACTUALIZA").value=persona.id;
+
+
+                
+            })
+        })
+    });   
+}
+const actualizar = ()=>{
+    //recupero datos
+    let enombre=document.getElementById("UPDnombre")
+    let eemail=document.getElementById("UPDemail")
+    let etelefono=document.getElementById("UPDtelefono")
+    let efecha=document.getElementById("UPDfecha")
+    let etiporeserva=document.getElementById("UPDtiporeserva")
+    let eacompañante;
+    if(document.getElementById("UPDacompañante1").checked){  //verifica con el checked 
+        eacompañante="1-2 personas"
+    }else if(document.getElementById("UPDacompañante2").checked){
+        eacompañante="3 o mas personas"
+    }
+    let ecomentarios = document.getElementById("UPDcomentario")
+    let emetodo;
+    if(document.getElementById("UPDcontacto").checked){
+        emetodo="Email";
+    }else if(document.getElementById("UPDcontacto2").checked){
+        emetodo="telefono";
+    }
+    
+    //recupero valores
+    let vnombre = enombre.value;
+    let vemail = eemail.value;
+    let vtelefono = etelefono.value;
+    let vfecha = efecha.value;
+    let vtiporeserva = etiporeserva.value;
+    let vacompañante = eacompañante;
+    let vcomentarios = ecomentarios.value;
+    let vmetodo = emetodo;
+    // objeto
+    let objeto={nombre:vnombre,email:vemail,telefono:vtelefono,fecha:vfecha,tiporeserva:vtiporeserva,eacompañante,vacompañante,
+        comentarios:vcomentarios,metodo:vmetodo
+    }
+    let id =document.getElementById("btnACTUALIZA").value;
+    actualizarPersona(objeto, id).then(()=>{
+        alert("se actualizo con exito");
+        cargarDatos();
+
+    })
 }
