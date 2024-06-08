ee1fa187e72ed7487cfd4e47470839889ce60975
scarent <jorgeee.g.a@gmail.com>
2024-06-08 16:57:22 -0400

    validaciones
    algunas validaciones terminadas, falta la validacion del checkbox,radio y select

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener("load",()=>{
     document.getElementById("btnmodoscuro").addEventListener("click",cambiarmodo);
     document.getElementById("btnfuente").addEventListener("click",cambiarfuente);
+    document.getElementById("btnregistro").addEventListener("click",validar);
+
 })
 function cambiarmodo(){
     var element =document.body;
@@ -9,4 +11,39 @@ function cambiarmodo(){
 function cambiarfuente(){
     document.body.classList.toggle("fontsize")
 
+}
+function validar(){
+    validarcampo('nombre')
+    validarcampo('email')
+    validarcampo('fecha')
+    validarcampo('comentario')
+    validarlargo('telefono',8)
+}
+function validarcampo(idCampo){
+    let inombre = document.getElementById(idCampo);
+    console.log(inombre)
+    let nombre = inombre.value;
+    console.log(nombre)
+    if(nombre.trim()==""){
+        inombre.style.borderColor="red";
+        let pnombre = document.getElementById("p"+idCampo);
+        pnombre.style.display ="block";    
+    }else{
+        inombre.style.borderColor="green";
+        let pnombre = document.getElementById("p"+idCampo);
+        pnombre.style.display="none"
+    }
+}
+function validarlargo(campo,largo){
+    let elemento = document.getElementById(campo);
+    let valor = elemento.value;
+    if(valor.length!=largo){
+        elemento.style.borderColor="red";
+        let pelemento=document.getElementById('p'+campo);
+        pelemento.style.display="block";
+    }else{
+        elemento.style.borderColor="green";
+        let pelemento=document.getElementById('p'+campo);
+        pelemento.style.display="none"
+    }
 }
